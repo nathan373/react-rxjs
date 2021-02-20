d79d3171947e8e9bae74883d900bf314ecfb6a4a
nathan373 <nmalink@gmail.com>
2021-02-20 11:08:28 -0500

    Initial commit

---
+++ src/_services/message.service.js
@@ -0,0 +1,11 @@
+import { Subject } from 'rxjs';
+
+const subject = new Subject();
+
+export const messageService = {
+    sendMessage: message => subject.next({ text: message }),
+    clearMessages: () => subject.next(),
+    getMessage: () => subject.asObservable()
+};
+
+import { Subject } from 'rxjs';
